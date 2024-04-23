64b2fd0b161716d4ef868db6a0692632494a1eab
starolyz <111028154+starolyz@users.noreply.github.com>
2024-04-23 10:13:04 -0500

    Phenomena

---
+++ My project/Assets/Scripts/SimpleAttach.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour
+{
+    private Interactable interactable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+
+    private void OnHandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        //GRAB THE OBJECT
+        if(interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+        }
+        //RELEASE
+        else if(isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+}
